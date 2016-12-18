0c50d0298fb455cd4a0b1166a06e373dd9b024eb
boostcorp <boostCorp3005>
2016-12-18 22:00:10 +0200

    kafedra parsing

--- ITF/AppDelegate.m
+++ ITF/AppDelegate.m
@@ -17,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+        [FIRApp configure];
         return YES;
 }
 
--- ITF/FacultyVC/FacultyVC.m
+++ ITF/FacultyVC/FacultyVC.m
@@ -1,3 +1,4 @@
+
 //
 //  FacultyVC.m
 //  ITF
@@ -12,30 +13,37 @@
 @interface FacultyVC ()
 @property (strong ,nonatomic) FIRDataSnapshot *snapShot;
 @property (strong ,nonatomic) FIRDatabaseReference *fir;
+@property (strong ,nonatomic) NSDictionary *ITFdict;
 @end
 
 @implementation FacultyVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [FIRApp configure];
-    self.fir = [[FIRDatabase database] reference];
 
-    [self.fir observeEventType:FIRDataEventTypeChildAdded withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
-        
-    }];
+    self.fir = [[FIRDatabase database] reference];
     
     [self.fir observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
-        
+        self.ITFdict = [snapshot.value objectForKey:@"ITF"];
+        [self.tableView reloadData];
     }];
     
-    
+}
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+-(void)generateUsers {
+    int count = arc4random()%30;
+    for (int j = 0 ; j < 5; j++) {
+        for (int i = 0 ; i <count ; i++) {
+            [[[[[[self.fir child:@"ITF"] child:@"TM"] child:@"Courses"] child:[NSString stringWithFormat:@"%d",j+1]]childByAutoId] setValue:@{@"Name":[self getRandName]} withCompletionBlock:^(NSError * _Nullable error, FIRDatabaseReference * _Nonnull ref) {
+                
+            }];
+        }
+    }
+}
+
+-(NSString *)getRandName {
+    NSArray *surNameArray = @[@"Abrahamovsky",@"Adamchuk",@"Andreichenko",@"Andriychuk",@"Andreichyn",@"Andreikiv",@"Andreiko",@"Andresyuk",@"Andrichuk",@"Andriyenko",@"Androshchuk",@"Andrukh",@"Andruko",@"Andruntsiv",@"Andrusenko",@"Andrushchenko",@"Andrushkiv",@"Andrushko",@"Andrusiv",@"Andrusyak",@"Andrusyshyn",@"Antonenko",@"Antoniw",@"Artyuschenko",@"Avramenko",@"Archipenko",@"Babiak",@"Bakaj",@"Balanchuk",@"Barabash",@"Belenko",@"Belovol",@"Bilokhatniuk",@"Bohatenko",@"Bondarenko",@"Boreckyi",@"Boyko",@"Brutka",@"Bublik",@"Bulhakow",@"Burgos",@"Bzovsky",@"Vann",@"Veremchuk",@"Vershigora",@"Vovk",@"Vynnychenko",@"Voitenko",@"Voloshyn",@"Vyhovsky",@"Vasilchuk",@"Harasemchuk",@"Halyckyj",@"Holub",@"Hordiyenko",@"Hotopylo",@"Huba",@"Hubenko",@"Hulyahrotsky",@"Hrytsenko",@"Hryshchuk",@"Holowaty",@"Hycha",@"Gogol",@"Gleba",@"Galey",@"Goraya",@"Gulka",@"Gura",@"Gurka",@"Glushenko",@"Gryglewicz",@"Dolouda",@"Danilenko",@"Dobryivechir",@"Doroshenko",@"Evanishyn",@"Ewaschuk",@"Yehorenko",@"Yelenyuk",@"Yeliashkevych",@"Yelyuk",@"Yevdokymenko",@"Yevtukh",@"Yevtushenko",@"Yevtushok",@"Yecoshenko",@"Zherdev",@"Yakimchuk",@"Yarema",@"Ivanenko",@"Yiranek",@"Yovenko",@"Kalashnik",@"Karaszkewycz",@"Karwatsky",@"Katyuk",@"Klimenko",@"Kluka",@"Kobevko",@"Korneluk",@"Kosh",@"Koshla",@"Kovalenko",@"Kovalchick",@"Kovalchuk",@"Kozachenko",@"Kozoriz",@"Kravets",@"Kryvobok",@"Kryvonis",@"Kryvoruchko",@"KrylenkoKudleychuk",@"Koziy",@"Koltasz",@"Koltyk",@"Lazarenko",@"Lukyanenko",@"Lukashenko",@"Leonchuk",@"Liski",@"Loboda",@"Lyakh",@"Ladanchuk",@"Makarenko",@"Mowczan",@"Mandyczewski",@"Marmurchuk",@"Maslak",@"Miroshnychenko",@"Motruk",@"Maryniuk",@"Mykhaylyk",@"Nazarenko",@"Nazdratenko"];
+    return [[surNameArray[arc4random()%surNameArray.count] stringByAppendingString:@" "] stringByAppendingString:surNameArray[arc4random()%surNameArray.count]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,15 +51,18 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cafedraCell" forIndexPath:indexPath];
+    cell.textLabel.text = [[self.ITFdict allKeys] objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [[[self.ITFdict allValues] objectAtIndex:indexPath.row] valueForKey:@"Name"];
     
     return cell;
 }
-*/
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.ITFdict.count;
+}
 
 /*
 // Override to support conditional editing of the table view.
